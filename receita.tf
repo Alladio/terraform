494d437068bf5ab65d485550c6d7c1fa032993c5
Alladio Bonesso <alladio_bonesso@hotmail.com>
2024-03-07 14:47:06 -0300

    Refact: + variables

--- receita.tf
+++ receita.tf
@@ -10,9 +10,9 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
-  access_key = "my-access-key"
-  secret_key = "my-secret-key"
+  region = var.aws_region
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 data "aws_ami" "ubuntu" {
